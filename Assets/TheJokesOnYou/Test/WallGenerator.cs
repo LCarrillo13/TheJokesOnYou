0d2d7646380e44f78e7eb323d43d248536f5433b
LCarrillo13 <78832132+LCarrillo13@users.noreply.github.com>
2021-09-14 16:28:42 +1000

    another Test commit

--- Assets/TheJokesOnYou/Test/WallGenerator.cs
+++ Assets/TheJokesOnYou/Test/WallGenerator.cs
@@ -11,11 +11,30 @@ namespace TheJokesOnYou.Mechanics
         [SerializeField] private GameObject wallPrefab3;
         [SerializeField] private GameObject wallPrefab4;
         [SerializeField] private int randomNum = 0;
+        [SerializeField] private Vector3 targetSpawn;
+        [SerializeField] private Vector3 targetOffset = Vector3.forward;
+        [SerializeField] private Vector3 quartTest = Vector3.one;
+        [SerializeField] private Quaternion spawnQuaternion;
+        [SerializeField] private Transform targetTransform;
 
+        void GenerateWall()
+        {
+            Instantiate(wallPrefab1, targetSpawn, spawnQuaternion);
+        }
         // Start is called before the first frame update
-        void Start() { }
+        void Start()
+        {
+            targetSpawn = transform.position + targetOffset;
+            spawnQuaternion = targetTransform.rotation;
+        }
 
         // Update is called once per frame
-        void Update() { }
+        void Update()
+        {
+            if(Input.GetKeyDown("space"))
+            {
+                GenerateWall();
+            }
+        }
     }
 }
