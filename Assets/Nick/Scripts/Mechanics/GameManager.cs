09ce08ef55f602b5b9675216842e5f223c4c5296
Nick12345-code <astonishedchocolate@gmail.com>
2021-11-06 16:01:06 +1100

    asdf

--- Assets/Nick/Scripts/Mechanics/GameManager.cs
+++ Assets/Nick/Scripts/Mechanics/GameManager.cs
@@ -5,10 +5,39 @@ using Mirror;
 public class GameManager : NetworkBehaviour
 {
     public enum Mode { Race, Survival, Mode3 }
+    public enum Map { Day, Night }
 
-    public Mode mode;
+    public static Mode mode;
+    public static Map map;
+
+    public Texture dayMap, nightMap;
+
+    Scene scene;
+
+    void Start()
+    {
+        scene = SceneManager.GetActiveScene();
+
+        // spawns a specific map depending on what map is selected
+        if (scene.name != "Lobby" || scene.name != "Room")
+        {
+            MeshRenderer temp = GameObject.Find("Quad - Sky").GetComponent<MeshRenderer>();
+
+            if (map == Map.Day)
+            {
+                temp.material.mainTexture = dayMap;
+            }
+            else if (map == Map.Night)
+            {
+                temp.material.mainTexture = nightMap;
+            }
+        }
+
+
+    }
 
     public void ChangeGameMode(int index) => mode = (Mode)index;
+    public void ChangeMap(int index) => map = (Map)index;
 
     // changes the scene for all clients
     public void ChangeScene(string name) => SceneManager.LoadScene(name);
--- Assets/Nick/Scripts/Networking/CustomNetworkRoomManager.cs
+++ Assets/Nick/Scripts/Networking/CustomNetworkRoomManager.cs
@@ -95,7 +95,7 @@ public class CustomNetworkRoomManager : NetworkRoomManager
     // The default implementation of this function uses ServerChangeScene() to switch to the game player scene. By implementing this callback you can customize what happens when all the players in the room are ready, such as adding a countdown or a confirmation for a group leader.
     public override void OnRoomServerPlayersReady()
     {
-        switch (manager.mode)
+        switch (GameManager.mode)
         {
             case GameManager.Mode.Race:
                 ServerChangeScene(raceScene);
@@ -103,10 +103,10 @@ public class CustomNetworkRoomManager : NetworkRoomManager
             case GameManager.Mode.Survival:
                 ServerChangeScene(survivalScene);
                 break;
-            default:
-                break;
             case GameManager.Mode.Mode3:
                 ServerChangeScene(thirdScene);
+                break;
+            default:
                 break;
         }
     }
